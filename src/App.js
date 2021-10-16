7dfedfcdd1802462acbb640fed814a7a63479c2d
charleslambdev <charleslambdev@penguin>
2021-10-16 10:20:15 -0400

    updated home screen message

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+Gutties Outlet coming soon!   <br/>   
+soon like next week....  </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         -Jamal Mattingly
         </a>
       </header>
     </div>
