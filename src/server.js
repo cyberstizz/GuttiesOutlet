05da6f2b7969b3271d96a984d74eb1fab36f78ed
charleslambdev <charleslambdev@penguin>
2021-10-22 14:40:11 -0400

    created test server code that will change after passing user tests

--- src/server.js
+++ src/server.js
@@ -1,2 +1,14 @@
+const express = require('express'); //pointing to express dependancy
+const app = express(); // initiation of server as app variable
+const port = process.env.PORT || 5000; // created port variable in case of environment variables
 
 
+// create a GET route
+app.get('/express_backend', (req, res) => { //Line 9
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
+}); //Line 11
+
+
+
+// below displays message that the server running and listening to specified port
+app.listen(port, () => console.log(`Listening on port ${port}`)); 
