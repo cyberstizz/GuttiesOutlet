221aa1a2a9514417ac75f09c4765ae0170d403d1
charleslambdev <charleslambdev@penguin>
2021-10-22 12:37:12 -0400

    completed products table

--- db.sql
+++ db.sql
@@ -1,10 +1,22 @@
 create table users(
 user_id varchar(20) primary key,
-user_password varchar(20),
-email_address unique,
-physical_address
+session_id integer unique not null,
+user_password varchar(20) not null,
+email_address unique not null,
+physical_address varchar(50)
 );
 
 create table products(
-    product_id primary key
+    product_id  integer primary key,
+    product_name varchar(50) unique not null,
+    product_price integer not null,
+    product_description varchar(200) not null,
+    product_quantity integer not null,
+    product_image_one bytea,
+    product_image_two bytea,
+    product_image_three bytea,
+    product_image_four bytea,
+    product_ratings integer,
+    product_delivery_time varchar(20) not null,
+    date_added date not null
 );
