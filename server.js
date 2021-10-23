32f8f02472339afaba3f9056ff244bec3e2ef039
charleslambdev <charleslambdev@penguin>
2021-10-23 13:12:15 -0400

    fixed server issue

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('this is working')
+})
+
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`)
 });
